0f8913b439818109529dcd2821a2276a14161df0
Sweetdesignhub <152193938+Sweetdesignhub@users.noreply.github.com>
2024-03-29 11:35:36 +0530

    Update yfinance_data.py

--- main/src/lib/yfinance_data.py
+++ main/src/lib/yfinance_data.py
@@ -9,11 +9,13 @@ Created on Tue Mar 26 17:53:47 2024
 import yfinance as yahooFinance
 import pandas as pd
 
-#url = "https://nsearchives.nseindia.com/content/indices/ind_nifty100list.csv"
-#s = requests.get(url).content
+
+
 pd.set_option('display.max_rows', None)
 
+#nifty100df = pd.read_csv("https://nsearchives.nseindia.com/content/indices/ind_nifty100list.csv")
 nifty100df = pd.read_csv("G:/Study/Algo_trade/Labs/data/nse/symbols/ind_nifty100list.csv")
+
 
 nifty100list = nifty100df["Symbol"]
 
@@ -21,4 +23,5 @@ for symbol in nifty100list:
     print(symbol+".NS")
     GetFacebookInformation = yahooFinance.Ticker(symbol+".NS")
     data = pd.DataFrame(GetFacebookInformation.history(period="max"))
+    #data.to_csv(symbol+".csv")
     data.to_csv("G:/Study/Algo_trade/Labs/data/nse/nifty100/"+symbol+".csv")
